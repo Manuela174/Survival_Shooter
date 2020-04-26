03b5da4eb8f9e9a407eea79eacc397bc04880fa7
Manuela174 <45518560+Manuela174@users.noreply.github.com>
2020-04-26 13:05:11 +0200

    ..
    ...

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraFollow: MonoBehaviour
+{
+    public Transform meta; //Položaj kojega kamera slijedi
+    public float filtriranje = 5f; //Brzina kojom kamera slijedi
+    Vector3 pomak; //Početno odmicanje od meta
+
+    void Start()
+    {
+        //Izračun početnog pomaka
+        pomak = transform.position -meta.position;
+    }
+
+    void FixedUpdate()
+    {//kreiranje pozicije kamere cilja na temelju meta do pomak
+        Vector3 targetCamPos = meta.position + pomak;
+
+        //Glatko procijeniti položaj kamere prema trenutnom položaju i meta položaju
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, filtriranje * Time.deltaTime);
+    }
+}
